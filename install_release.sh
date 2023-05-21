bd5e6c39fe81e6abaa5bdfc2e98caf83594eaedb
kubepy <43437760+kubepy@users.noreply.github.com>
2023-05-21 23:30:56 +0800

    Update install_release.sh

--- install_release.sh
+++ install_release.sh
@@ -3,13 +3,17 @@
 # holo-gs-auto-update
 yay -Sy holo-gs-auto-update --noconfirm
 if [[ "$?" == "0" ]]; then
-        yay_status=更新完毕else
-        yay_status=更新失败fi
+	yay_status=更新完毕
+else
+	yay_status=更新失败
+fi
 
 # decky
 curl -Lk https://github.com/SteamDeckHomebrew/decky-installer/releases/latest/download/install_release.sh | sed 's#prerelease == "false"#prerelease == "true"#' | sh
 if [[ "$?" == "0" ]]; then
-        decky_status=更新完毕else
-        decky_status=更新失败fi
+        decky_status=更新完毕
+else
+	decky_status=更新失败
+fi
 
 zenity --warning --text="软件包：holo-gs-auto-update $yay_status \n插件：decky $decky_status \n全部更新脚本已执行完毕" --width=100 --height=100
