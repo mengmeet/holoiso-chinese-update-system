d42302ff1b01fafb38b148885fa2a13b1316091d
kubepy <43437760+kubepy@users.noreply.github.com>
2023-06-03 21:00:47 +0800

    Update install_release.sh

--- install_release.sh
+++ install_release.sh
@@ -10,11 +10,19 @@ else
 fi
 
 # decky
-curl -Lk https://github.com/SteamDeckHomebrew/decky-installer/releases/latest/download/install_release.sh | sed 's#prerelease == "false"#prerelease == "true"#' | sudo sh
+curl -Lk https://download.ohmydeck.net/install.sh | sed 's#prerelease == "false"#prerelease == "true"#' | sudo sh
 if [[ "$?" == "0" ]]; then
         decky_status=更新完毕
 else
 	decky_status=更新失败
 fi
 
-zenity --warning --text="软件包：holo-gs-auto-update $yay_status \n插件：decky $decky_status \n全部更新脚本已执行完毕" --width=100 --height=100
+# tomoon
+curl -L http://i.ohmydeck.net | sh
+if [[ "$?" == "0" ]]; then
+        tomoon_status=更新完毕
+else
+	tomoon_status=更新失败
+fi
+
+zenity --warning --text="软件包：holo-gs-auto-update $yay_status \n插件：decky $decky_status \n插件软件：toomon $tomoon_status \n全部更新脚本已执行完毕" --width=100 --height=100
